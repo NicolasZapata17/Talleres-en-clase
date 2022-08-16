bfeb8748872af54850c5a7284351e181b79027e3
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-08-16 08:41:23 -0500

    Se realizo el examen

---
+++ Taller-2/Examen-condicionales.js
@@ -0,0 +1,16 @@
+var masa = 50;
+var planeta = "Marte"
+var grav_marte = 3.711;
+var grav_tierra = 9.81;
+var peso1 = masa * grav_marte;
+var peso2 = masa * grav_tierra;
+
+if(planeta == "Marte"){
+    console.log("Su peso en marte es: " + peso1.toFixed(4))
+}else{
+    if(planeta == "Tierra"){
+        console.log("Su peso en la tierra es: " + peso2.toFixed(4))
+    }else{
+        console.log("Ingrese un valor adecuado")
+    }
+}
