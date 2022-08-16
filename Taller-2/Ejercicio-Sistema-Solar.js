3ed8b61310818ff68377737a6573b028596c8635
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-08-15 22:46:50 -0500

    Actualice archivo del sistema solar

--- Taller-2/Ejercicio-Sistema-Solar.js
+++ Taller-2/Ejercicio-Sistema-Solar.js
@@ -5,31 +5,31 @@ const respuesta = "El planeta es ";
 
 
 if(pocision==1){
-    console.log(respuesta + "Mercurio")
+    console.log(respuesta + "Mercurio"+ " " + "esta en la posición: 1")
 }else{
     if(pocision==2){
-        console.log(respuesta + "Venus")
+        console.log(respuesta + "Venus"+ " " + "esta en la posición: 2")
     }else{
         if(pocision==3){
-            console.log(respuesta + "Tierra")
+            console.log(respuesta + "Tierra"+ " " + "esta en la posición: 3")
         }else{
             if(pocision==4){
-                console.log(respuesta + "Marte")
+                console.log(respuesta + "Marte"+ " " + "esta en la posición: 4")
             }else {
                 if(pocision==5){
-                    console.log(respuesta + "Jupiter")
+                    console.log(respuesta + "Jupiter"+ " " + "esta en la posición: 5")
                 }else{
                     if(pocision==6){
-                        console.log(respuesta + "Saturno")
+                        console.log(respuesta + "Saturno"+ " " + "esta en la posición: 6")
                     }else{
                         if(pocision==7){
-                            console.log(respuesta + "Urano")
+                            console.log(respuesta + "Urano"+ " " + "esta en la posición: 7")
                         }else{
                             if(pocision==8){
-                                console.log(respuesta + "Neptuno")
+                                console.log(respuesta + "Neptuno"+ " " + "esta en la posición: 8")
                             }else{
                                 if(pocision==9){
-                                    console.log(respuesta + "Pluton")
+                                    console.log(respuesta + "Pluton"+ " " + "esta en la posición: 9")
                                 }else{
                                     console.log("Ingresa un valor valido")
                                 }
