ce745adf1ce44eee71ae4513d5bccaf53d39bf87
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-08-18 09:31:17 -0500

    Nuevos ejercicios en clase

--- Taller-2/Ejercicio-tienda.js
+++ Taller-2/Ejercicio-tienda.js
@@ -3,7 +3,6 @@
 //var desc2 = ValorCompra*0.10;
 //var total1 = ValorCompra-desc1;
 //var total2 = ValorCompra-desc2;
-
 //if (ValorCompra>=80.000 ){
 //    console.log("Su Valor total de la compra es: " + total1.toFixed(3))
 //}else{
@@ -13,6 +12,8 @@
 //        console.log("No aplica ningún descuento")
 //    }
 //}
+
+
 //En la tienda ABC se genera un descuento del 5% si las compras superan el valor
 //de $80.000 y se otorga el 10% cuando las ventas superan $250.000
 
