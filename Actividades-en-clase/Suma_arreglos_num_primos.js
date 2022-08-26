40926d416b6741e134732376650d9cd6e6e985a7
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-08-26 14:53:42 -0500

    nuevos ejercicios

---
+++ Actividades-en-clase/Suma_arreglos_num_primos.js
@@ -0,0 +1,14 @@
+let arreglo = [4, 3, 9, 7, 12];
+let arreglo_2 = [1, 2, 5, 8, 11];
+let n = arreglo.length;
+let suma = 0;
+
+for(i=0; i<n; i++){
+    suma=arreglo[i]+arreglo_2[i];
+    if(suma % i == 0){
+        console.log("No es numero primo " + suma)
+    }else{
+        console.log("Es numero primo " + suma)
+    }
+}
+
