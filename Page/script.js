ec75245525dbfae5481a222ecea2bd0dfe0cbf09
NicolasZapata17 <zapataacostanicolasalejandro@gmail.com>
2022-09-02 11:18:38 -0500

    Add new functions to JS

--- Page/script.js
+++ Page/script.js
@@ -10,9 +10,23 @@ function bebida(){
     var pepsi = document.getElementById("radio2")
     var postobon = document.getElementById("radio3")
     var Calorias = document.getElementById("Calorias")
+    var Precio = document.getElementById("Precio")
+    
 
     if (cocaCola.checked) {
-        Calorias.innerHTML = "Las calorias de la CocaCola son: 100"
+        Calorias.innerHTML = `<label>¿CocaCola Zero?</label>
+        <br>
+        <label><input type='radio' name='cocaColaName' id="CocaColaZero" class='form-check-input'>si</label>
+        <label><input type='radio' name='cocaColaName' id="CocaColaTradicional" class='form-check-input'>no</label>`;
+        
+        // Precio.innerHTML = `<p>El precio de la tradicional es: 5000</p> `
+        // <input type='radio' name='a' class='form-check-input'
+        // <label class="form-check-label" >Tradicional</label>
+        // <h5>El precio de la tradicional es: 5000<h5>
+
+        // <input type='radio' name='a' class='form-check-input'
+        // <label class="form-check-label" >Cero</label>
+        // <h5>El precio de la cero es: 4000<h5></h5>
     }else{
         if (pepsi.checked) {
             Calorias.innerHTML = "Las calorias de la Pepsi son: 300"
@@ -21,6 +35,8 @@ function bebida(){
     }
     }
 }
+
+    // Uso de alerts
     // if (cocaCola.checked) {
     //     alert("Usted selecciono Coca-Cola")
     // }else{
